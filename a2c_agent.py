Fixing a few issues with the PPO loss calculation and refactoring the learning agent shared code a bit better

a2c_agent.py
@@ -15,17 +15,23 @@ from starcraft_agents.fully_conv_model import FullyConvModel
 from starcraft_agents.learning_agent import LearningAgent
 from starcraft_agents.saved_actions import SavedActions
 
-expirement_name = "collect_minerals"
+expirement_name = "pr_a2c_lings_2"
 
 class A2CAgent(LearningAgent):
     """The start of a basic A2C agent for learning agents."""
-    def __init__(self, screen_width=84, screen_height=84, num_steps=64, num_processes=1, fully_conv=False):
-        super(A2CAgent, self)
+    def __init__(self, screen_width=64, screen_height=64, num_steps=40, num_processes=1, fully_conv=False, expirement_name=expirement_name):
+        super(A2CAgent, self).__init__(create_experiment)
         num_functions = len(actions.FUNCTIONS)
         if fully_conv:
-            self.model = FullyConvModel(num_functions=num_functions).cuda()
+            self.model = FullyConvModel(num_functions=num_functions,
+                                        expirement_name=expirement_name,
+                                        screen_width=screen_width,
+                                        screen_height=screen_height).cuda()
         else:
-            self.model = A2CModel(num_functions=num_functions, expirement_name=expirement_name).cuda()
+            self.model = A2CModel(num_functions=num_functions,
+                                  expirement_name=expirement_name,
+                                  screen_width=screen_width,
+                                  screen_height=screen_height).cuda()
 
         self.screen_width = screen_width
         self.screen_height = screen_height
@@ -42,61 +48,13 @@ class A2CAgent(LearningAgent):
                                           self.num_processes,
                                           num_functions)
         self.saved_actions.cuda()
-        self.rollout_step = 0
         #self.model.load_state_dict(torch.load(f"./{expirement_name}.pth"))
         #self.model.eval()
         self.optimizer = optim.RMSprop(self.model.parameters(),
                                         lr=7e-4,
                                         eps=1e-10,
                                         alpha=0.99)
-        self.episode_rewards = torch.zeros(1, 1)
         self.final_rewards = torch.zeros(1, 1)
-
-    def step(self, obs):
-        super(A2CAgent, self).step(obs)
-        self.rollout_step += 1
-
-        reward = torch.from_numpy(np.expand_dims(obs.reward, 1)).float()
-        self.episode_rewards += reward
-
-        screen, minimap, game, allowed_actions = self.preprocess(obs)
-
-        value_pred, action, x1, y1 = self.model.act(Variable(screen).cuda(),
-                                                    Variable(minimap).cuda(),
-                                                    Variable(game).cuda(),
-                                                    Variable(allowed_actions).cuda())
-        function_id = action.data[0]
-
-        act_args = []
-        for args in actions.FUNCTIONS[function_id].args:
-            if args.name in ['screen', 'minimap', 'screen2']:
-                act_args.append([x1.data[0,0], y1.data[0,0]])
-            else:
-                act_args.append([0])
-
-        masks = torch.FloatTensor([0.0] if obs.step_type == 2 else [1.0])
-
-        self.final_rewards *= masks
-        self.final_rewards += (1 - masks) * self.episode_rewards
-
-        self.saved_actions.insert(self.rollout_step, screen, minimap, game, action.data, x1.data, y1.data, value_pred.data, reward, masks)
-
-        if self.rollout_step == self.num_steps:
-            self.rollout()
-
-        return actions.FunctionCall(function_id, act_args)
-
-    def reset(self):
-        super(A2CAgent, self).reset()
-        print(f"Episode Reward was: {self.episode_rewards[0,0]}")
-        self.model.summary.add_scalar_value("episode_reward", int(self.episode_rewards[0,0]))
-        self.episode_rewards = torch.zeros(1, 1)
-
-        if self.steps > 1:
-            self.rollout()
-
-        torch.save(self.model.state_dict(), f"./{expirement_name}.pth")
-        self.saved_actions.reset()
 
     def rollout(self):
         if self.rollout_step < 1:
@@ -116,8 +74,9 @@ class A2CAgent(LearningAgent):
 
         values, logpac, entropy = self.model.evaluate_actions(screens, minimaps, games, actions, x1s, y1s)
 
-        pg_loss = -(Variable(advantages).cuda() * logpac).mean()
-        pg_loss = pg_loss - self.entropy_coef * entropy
+        dist_entropy = self.entropy_coef * entropy.mean()
+        pg_loss = (Variable(advantages).cuda() * logpac).mean()
+        pg_loss = pg_loss - dist_entropy
         vf_loss = advantages.pow(2).mean()
 
         train_loss = pg_loss + self.value_coef * vf_loss
@@ -132,6 +91,7 @@ class A2CAgent(LearningAgent):
         self.model.summary.add_scalar_value("reward", int(self.reward))
         self.model.summary.add_scalar_value("value_loss", vf_loss)
         self.model.summary.add_scalar_value("loss", train_loss.data[0])
+        self.model.summary.add_scalar_value("dist_entropy", dist_entropy.data[0])
 
         # if we are still running an episode, copy over the state to position zero
         last_full_action = self.rollout_step

a2c_model.py
@@ -13,8 +13,9 @@ from collections import namedtuple
 
 
 class A2CModel(nn.Module):
-    def __init__(self, num_functions, expirement_name, screen_width=84, screen_height=84):
+    def __init__(self, num_functions, expirement_name, screen_width, screen_height):
         super(A2CModel, self).__init__()
+        self.neglogp= nn.CrossEntropyLoss(reduce=False)
         self.num_functions = num_functions
         self.screen_width = screen_width
         self.screen_height = screen_height
@@ -28,20 +29,20 @@ class A2CModel(nn.Module):
         self.summary = self.cc.create_experiment(expirement_name)
 
         # our model specification
-        self.conv1 = nn.Conv2d(13, 16, kernel_size=8, stride=4)
+        self.conv1 = nn.Conv3d(1, 16, kernel_size=8, stride=4)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
 
-        self.mm_conv1 = nn.Conv2d(7, 16, kernel_size=8, stride=4)
+        self.mm_conv1 = nn.Conv3d(1, 16, kernel_size=8, stride=4)
         self.mm_conv2 = nn.Conv2d(16, 32, kernel_size=4, stride=2)
 
         self.feature_input = nn.Linear(11, 128)
 
-        self.fc = nn.Linear(5312, 512)
+        self.fc = nn.Linear(2432, 512)
         self.action_head = nn.Linear(512, self.num_functions)
         self.value_head = nn.Linear(512, 1)
 
-        self.spatial_x = nn.Linear(512, 84)
-        self.spatial_y = nn.Linear(512, 84)
+        self.spatial_x = nn.Linear(512, screen_width)
+        self.spatial_y = nn.Linear(512, screen_height)
 
         self.saved_actions = []
         self.rewards = []
@@ -49,19 +50,19 @@ class A2CModel(nn.Module):
     def forward(self, screen, mm, game):
         # screen network
         screen = F.elu(self.conv1(screen))
-        screen = F.elu(self.conv2(screen))
+        screen = F.elu(self.conv2(screen.squeeze(2)))
         #x = F.relu(self.conv3(x))
 
         # minimap network
         mm = F.elu(self.mm_conv1(mm))
-        mm = F.elu(self.mm_conv2(mm))
+        mm = F.elu(self.mm_conv2(mm.squeeze(2)))
         #mm = F.relu(self.mm_conv3(mm))
 
         #non spatial structured data
         xs = F.tanh(self.feature_input(game))
 
         # non-spatial policy
-        st = torch.cat([screen.view(-1, 2592), mm.view(-1, 2592), xs], dim=1)
+        st = torch.cat([screen.view(-1, 1152), mm.view(-1, 1152), xs], dim=1)
         ns = F.relu(self.fc(st))
 
         # critic prediction
@@ -81,31 +82,35 @@ class A2CModel(nn.Module):
                                                          minimap,
                                                          game_state)
 
-        action = F.softmax(action_logits.squeeze(0).gather(0, available_actions)).multinomial()
-        x1 = F.softmax(x1_logits).multinomial()
-        y1 = F.softmax(y1_logits).multinomial()
+        action = F.softmax(action_logits.squeeze(0).gather(0, available_actions), dim=0).multinomial()
+        x1 = F.softmax(x1_logits, dim=0).multinomial()
+        y1 = F.softmax(y1_logits, dim=0).multinomial()
 
         return state_value, available_actions[action], x1, y1
+
+    def entropy(self, logits):
+        a0 = logits - torch.max(logits)
+        ea0 = torch.exp(a0)
+        z0 = ea0.sum(-1, keepdim=True)
+        p0 = ea0 / z0
+        return (p0 * (torch.log(z0) - a0)).sum(-1)
 
     def evaluate_actions(self, screens, minimaps, games, actions, x1s, y1s):
         logits, x1_logits, y1_logits, values = self(screens, minimaps, games)
 
-        log_probs = F.log_softmax(logits)
-        probs = F.softmax(logits)
-        action_log_probs = log_probs.gather(1, actions)
-        dist_entropy = (log_probs * probs).sum(1).mean()
+        log_probs = F.log_softmax(logits, dim=1)
+        action_nlp = self.neglogp(logits, actions.squeeze(1))
+        dist_entropy = self.entropy(logits)
 
-        x1_log_probs = F.log_softmax(x1_logits)
-        x1_probs = F.softmax(x1_logits)
-        x1_act_lp = (x1_log_probs * x1_probs).gather(1, x1s)
-        x1_entropy = (x1_log_probs * x1_probs).sum(1).mean()
+        x1_log_probs = F.log_softmax(x1_logits, dim=1)
+        x1_nlp = self.neglogp(x1_logits, x1s.squeeze(1))
+        x1_entropy = self.entropy(x1_logits)
 
-        y1_log_probs = F.log_softmax(y1_logits)
-        y1_probs = F.softmax(y1_logits)
-        y1_act_lp = (y1_log_probs * y1_probs).gather(1, y1s)
-        y1_entropy = (y1_log_probs * y1_probs).sum(1).mean()
+        y1_log_probs = F.log_softmax(y1_logits, dim=1)
+        y1_nlp = self.neglogp(y1_logits, y1s.squeeze(1))
+        y1_entropy = self.entropy(y1_logits)
 
-        logpac = action_log_probs + x1_act_lp + y1_act_lp
+        neg_logpac = action_nlp + x1_nlp + y1_nlp
         entropy = dist_entropy + x1_entropy + y1_entropy
 
-        return values, logpac, entropy
+        return values, neg_logpac, entropy

fully_conv_model.py
@@ -10,14 +10,13 @@ from collections import namedtuple
 
 
 class FullyConvModel(nn.Module):
-    def __init__(self, num_functions, screen_width=84, screen_height=84):
+    def __init__(self, num_functions, expirement_name, screen_width=64, screen_height=64):
         super(FullyConvModel, self).__init__()
         self.num_functions = num_functions
         self.screen_width = screen_width
         self.screen_height = screen_height
 
         self.cc = CrayonClient(hostname="localhost")
-        expirement_name = type(self).__name__
 
         # if we have an existing expirement with this name, clean it up first
         if expirement_name in self.cc.get_experiment_names():
@@ -66,9 +65,10 @@ class FullyConvModel(nn.Module):
 
         # need to predict the spatial action policy here
         spatial = self.spatial_policy(st)
+        flat_yx = spatial.view(-1, self.screen_height * self.screen_width)
 
         return (action_scores,
-                spatial.view(-1, self.screen_height * self.screen_width),
+                flat_yx // screen_width, flat_yx % screen_height,
                 state_values)
 
     def act(self, state, minimap, game_state, available_actions):
@@ -78,21 +78,25 @@ class FullyConvModel(nn.Module):
 
         action = F.softmax(action_logits.squeeze(0).gather(0, available_actions)).multinomial()
         spatial = F.softmax(spatial_probs).multinomial()
-
         return state_value, available_actions[action], spatial
 
-    def evaluate_actions(self, screens, minimaps, games, actions):
-        logits, spatial_logits, values = self(screens, minimaps, games)
+    def evaluate_actions(self, screens, minimaps, games, actions, x1s, y1s):
+        def recover_yx_index(xs, ys):
+            return [y * self.screen_height + x for x,y in zip(xs, ys)]
 
-        log_probs = F.log_softmax(logits)
-        probs = F.softmax(logits)
+        logits, x1_logits, y1_logits, values = self(screens, minimaps, games)
+
+        log_probs = F.log_softmax(logits, dim=1)
+        probs = F.softmax(logits, dim=1)
         action_log_probs = log_probs.gather(1, actions)
         dist_entropy = (log_probs * probs).sum(1).mean()
 
-        spatial_log_probs = F.log_softmax(spatial_logits)
-        spatial_probs = F.softmax(spatial_logits)
-        spatial_act_log_probs = spatial_log_probs.gather(1, actions)
-        spatial_dist_entropy = (spatial_log_probs * spatial_probs).sum(1).mean()
+        spatial_log_probs = F.log_softmax(x1_logits, dim=1)
+        spatil_probs = F.softmax(x1_logits, dim=1)
+        spatial_act_lp = (x1_log_probs * x1_probs).gather(1, x1s)
+        spatial_entropy = (x1_log_probs * x1_probs).sum(1).mean()
 
-        return (values, action_log_probs, dist_entropy,
-                       spatial_act_log_probs, spatial_dist_entropy)
+        logpac = action_log_probs + spatial_act_lp
+        entropy = dist_entropy + spatial_entropy
+
+        return values, logpac, entropy

learning_agent.py
@@ -14,8 +14,28 @@ from torch.autograd import Variable
 from pycrayon import CrayonClient
 
 class LearningAgent(base_agent.BaseAgent):
-    def __init__(self):
-        super(LearningAgent, self)
+
+    def build_embeddings(self, feature_space, embedding_dim=5):
+        """
+        Build up a list of the embeddings for each layer in the supplied
+        feature collections
+
+        @param feature_space a feature.Feature constant from pysc2
+               either SCREEN_FEATURES or MINIMAP_FEATURES
+
+        @param embedding_dim how many dimensions to encode the categorical
+               features into
+        """
+        return [nn.Embedding(layer.scale, embedding_dim) for layer in feature_space]
+
+    def __init__(self, expirement_name):
+        super(LearningAgent, self).__init__()
+        self.expirement_name = expirement_name
+        self.rollout_step = 0
+        self.episode_rewards = torch.zeros(1, 1)
+
+        self.embeddings = {"screen": self.build_embeddings(features.SCREEN_FEATURES),
+                           "minimap": self.build_embeddings(features.MINIMAP_FEATURES)}
 
     def preprocess(self, obs):
         screen = obs.observation["screen"]
@@ -23,10 +43,10 @@ class LearningAgent(base_agent.BaseAgent):
         game = obs.observation["player"]
         allowed_actions = obs.observation["available_actions"]
 
-        screen = self.process(screen).unsqueeze(0)
-        minimap = self.process(minimap, features.MINIMAP_FEATURES).unsqueeze(0)
-        #screen = torch.from_numpy(screen).float().unsqueeze(0)
-        #minimap = torch.from_numpy(minimap).float().unsqueeze(0)
+        #screen = self.process(screen).unsqueeze(0)
+        #minimap = self.process(minimap, features.MINIMAP_FEATURES).unsqueeze(0)
+        screen = self.embed(screen, space="screen").unsqueeze(0)
+        minimap = self.embed(minimap, space="minimap").unsqueeze(0)
         game = torch.log(torch.from_numpy(game).float().unsqueeze(0))
 
         return screen, minimap, game, torch.from_numpy(allowed_actions).long()
@@ -47,15 +67,55 @@ class LearningAgent(base_agent.BaseAgent):
 
         return layers
 
-    def embed(self, feature_layers, embedding_dim=5, feature_space=features.SCREEN_FEATURES):
-         layer_count, screen_width, screen_height = feature_layers.shape
-         layers = torch.zeros(layer_count, 5, screen_width, screen_height)
+    def embed(self, feature_layers, embedding_dim=5, space="screen"):
+        layer_count, screen_width, screen_height = feature_layers.shape
+        layers = torch.zeros(1, embedding_dim, screen_width, screen_height)
+        feature_space = features.SCREEN_FEATURES if space == "screen" else features.MINIMAP_FEATURES
 
-         for i in range(layer_count):
-             embedding = nn.Embedding(feature_space[i].scale, 5)
-             embedded = embedding(Variable(torch.from_numpy(feature_layers[i])).long()).cuda()
-             e = embedded.view(-1, 5, screen_width, screen_height)
+        for i in range(layer_count):
+            # quick filter to only include the core layers needed to bootstrap an agent
+            if feature_space[i].name == "player_relative":
+                embedding = self.embeddings[space][i]
+                embedded = embedding(Variable(torch.from_numpy(feature_layers[i])).long()).cuda()
+                layers[0].copy_(embedded.data)
+        return layers
 
-             layers[i].copy_(e.data)
+    def step(self, obs):
+        super(LearningAgent, self).step(obs)
+        self.rollout_step += 1
 
-         return layers
+        reward = torch.from_numpy(np.expand_dims(obs.reward, 1)).float()
+        self.episode_rewards += reward
+
+        screen, minimap, game, allowed_actions = self.preprocess(obs)
+
+        value_pred, action, x1, y1 = self.model.act(Variable(screen).cuda(),
+                                                     Variable(minimap).cuda(),
+                                                     Variable(game).cuda(),
+                                                     Variable(allowed_actions).cuda())
+        function_id = action.data[0]
+        act_args = []
+        for args in actions.FUNCTIONS[function_id].args:
+            if args.name in ['screen', 'minimap', 'screen2']:
+                act_args.append([x1.data[0,0], y1.data[0,0]])
+            else:
+                act_args.append([0])
+
+        mask = torch.FloatTensor([0.0] if obs.step_type == 2 else [1.0])
+        self.saved_actions.insert(self.rollout_step, screen, minimap, game, action.data, x1.data, y1.data, value_pred.data, reward, mask)
+
+        if self.rollout_step == self.horizon:
+            self.rollout()
+
+        return actions.FunctionCall(function_id, act_args)
+
+    def reset(self):
+        super(LearningAgent, self).reset()
+        self.model.summary.add_scalar_value("episode_reward", int(self.episode_rewards[0,0]))
+        self.episode_rewards = torch.zeros(1, 1)
+
+        if self.steps > 1:
+            self.rollout()
+
+        torch.save(self.model.state_dict(), f"./{self.expirement_name}.pth")
+        self.saved_actions.reset()

ppo_agent.py
@@ -15,12 +15,13 @@ from starcraft_agents.ppo_model import PPOModel
 from starcraft_agents.learning_agent import LearningAgent
 from starcraft_agents.saved_actions import SavedActions
 
-expirement_name = "ppo_zerglings_pre"
+expirement_name = "pr_ppo_lings_5"
+
 
 class PPOAgent(LearningAgent):
     """The start of a basic PPO agent for Starcraft."""
-    def __init__(self, screen_width=84, screen_height=84, horizon=64, num_processes=1):
-        super(PPOAgent, self)
+    def __init__(self, screen_width=64, screen_height=64, horizon=40, num_processes=1, expirement_name=expirement_name):
+        super(PPOAgent, self).__init__(expirement_name)
         num_functions = len(actions.FUNCTIONS)
         self.model = PPOModel(num_functions=num_functions, expirement_name=expirement_name).cuda()
         self.old_model = copy.deepcopy(self.model)
@@ -28,10 +29,9 @@ class PPOAgent(LearningAgent):
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.horizon = horizon
-        self.rollout_step = 0
         #self.model.load_state_dict(torch.load(f"./{expirement_name}.pth"))
         #self.model.eval()
-        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)
         self.gamma = 0.99
         self.tau = 0.97
         self.entropy_coef = 0.01
@@ -41,47 +41,6 @@ class PPOAgent(LearningAgent):
                                           num_functions)
         self.saved_actions.cuda()
         self.episode_rewards = torch.zeros(1, 1)
-
-    def step(self, obs):
-        super(PPOAgent, self).step(obs)
-        self.rollout_step += 1
-
-        reward = torch.from_numpy(np.expand_dims(obs.reward, 1)).float()
-        self.episode_rewards += reward
-
-        screen, minimap, game, allowed_actions = self.preprocess(obs)
-
-        value_pred, action, x1, y1 = self.model.act(Variable(screen).cuda(),
-                                                    Variable(minimap).cuda(),
-                                                    Variable(game).cuda(),
-                                                    Variable(allowed_actions).cuda())
-        function_id = action.data[0]
-        act_args = []
-        for args in actions.FUNCTIONS[function_id].args:
-            if args.name in ['screen', 'minimap', 'screen2']:
-                act_args.append([x1.data[0,0], y1.data[0,0]])
-            else:
-                act_args.append([0])
-
-        mask = torch.FloatTensor([0.0] if obs.step_type == 2 else [1.0])
-        self.saved_actions.insert(self.rollout_step, screen, minimap, game, action.data, x1.data, y1.data, value_pred.data, reward, mask)
-
-        if self.rollout_step == self.horizon:
-            self.rollout()
-
-        return actions.FunctionCall(function_id, act_args)
-
-    def reset(self):
-        super(PPOAgent, self).reset()
-        print(f"Episode Reward was: {self.episode_rewards[0,0]}")
-        self.model.summary.add_scalar_value("episode_reward", int(self.episode_rewards[0,0]))
-        self.episode_rewards = torch.zeros(1, 1)
-
-        if self.steps > 1:
-            self.rollout()
-
-        torch.save(self.model.state_dict(), f"./{expirement_name}.pth")
-        self.saved_actions.reset()
 
     def rollout(self):
         if self.rollout_step < 1:
@@ -99,24 +58,26 @@ class PPOAgent(LearningAgent):
         x1s = Variable(st.x1s[:self.rollout_step].squeeze(1)).cuda()
         y1s = Variable(st.y1s[:self.rollout_step].squeeze(1)).cuda()
 
-        values, action_log_probs, dist_entropy = self.model.evaluate_actions(screens, minimaps, games, actions, x1s, y1s)
-        _, old_action_log_probs, _ = self.old_model.evaluate_actions(screens, minimaps, games, actions, x1s, y1s)
+        values, new_neglogp, dist_entropy = self.model.evaluate_actions(screens, minimaps, games, actions, x1s, y1s)
+        _, old_neglogp, _ = self.old_model.evaluate_actions(screens, minimaps, games, actions, x1s, y1s)
 
-        ratio = torch.exp(action_log_probs - Variable(old_action_log_probs.data))
+        ratio = torch.exp(new_neglogp - old_neglogp)
         adv_targ = Variable(advantages.view(-1, 1)).cuda()
         surr1 = ratio * adv_targ
         surr2 = torch.clamp(ratio, 1.0 - self.clip_param, 1.0 + self.clip_param) * adv_targ
         action_loss = -torch.min(surr1, surr2).mean() # PPO's pessimistic surrogate (L^CLIP)
+        policy_entp = (-self.entropy_coef) * dist_entropy.mean()
 
-        value_loss = (Variable(st.returns[:self.rollout_step]).cuda() - values).pow(2).mean()
+        value_loss = (values - Variable(st.returns[:self.rollout_step]).cuda()).pow(2).mean()
 
         self.optimizer.zero_grad()
-        total_loss = (value_loss + action_loss - dist_entropy * self.entropy_coef)
+        total_loss = (value_loss + action_loss + policy_entp)
         total_loss.backward()
         self.optimizer.step()
 
         self.model.summary.add_scalar_value("reward", int(self.reward))
         self.model.summary.add_scalar_value("loss", total_loss.data[0])
-        self.model.summary.add_scalar_value("dist_entropy", dist_entropy.data[0])
+        self.model.summary.add_scalar_value("dist_entropy", policy_entp.data[0])
+        self.model.summary.add_scalar_value("value_loss", value_loss.data[0])
 
         self.rollout_step = 0